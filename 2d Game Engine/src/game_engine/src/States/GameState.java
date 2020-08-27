bb6f8866440330c7065309c2e6d580b1e5bb7382
jacob <jacob@LAPTOP-2J3G99LR>
2020-08-27 17:22:12 -0500

    Added a resource class library and functional movement for rigby.

--- 2d Game Engine/src/game_engine/src/States/GameState.java
+++ 2d Game Engine/src/game_engine/src/States/GameState.java
@@ -2,13 +2,16 @@ package game_engine.src.States;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 
+import game_engine.src.Graphics.ImageLoader;
 import game_engine.src.Main.Game;
 
 public class GameState extends State 
 {
 	
 	private Game game;
+	private BufferedImage rigbone;
 	private int x, y;
 
 	public GameState(Game game)
@@ -16,37 +19,36 @@ public class GameState extends State
 		this.game = game;
 		x = 100;
 		y = 100;
+		rigbone = ImageLoader.loadImage("/textures/rigby.png");
 	}
 	
 	@Override
 	public void tick() 
 	{
-		if(game.getKeyManager().up == true)
-		{
-			y-=2;
-		}
-		if(game.getKeyManager().down == true)
-		{
-			y+=2;
-		}
-		if(game.getKeyManager().left == true)
-		{
-			x-=2;
-		}
-		if(game.getKeyManager().right == true)
-		{
-			x+=2;
-		}
-			
+			if(game.getKeyManager().up)
+			{
+				y -= 2;
+			}
+			if(game.getKeyManager().down)
+			{
+				y += 2;
+			}
+			if(game.getKeyManager().left)
+			{
+				x -= 2;
+			}
+			if(game.getKeyManager().right)
+			{
+				x += 2;
+			}
 	}
 
 	@Override
 	public void render(Graphics g) 
 	{
-		g.setColor(Color.red);
-		g.fillRect(0, 0, game.width, game.height);
 		g.setColor(Color.blue);
-		g.fillRect(x, y, 50, 50);
+		g.fillRect(0, 0, game.width, game.height);
+		g.drawImage(rigbone, x, y, 128, 128, null);
 	}
 
 }
